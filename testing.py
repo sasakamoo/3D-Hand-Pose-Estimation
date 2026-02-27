4fcba08cc6260747ef15d6a4d816c0ededdce8ca
Matthew Sasakamoose <matt.sasakamoose@gmail.com>
2026-02-27 16:58:37 -0700

    simple UNet architecture

--- testing.py
+++ testing.py
@@ -29,8 +29,8 @@ class FreiHAND_Dataset(Dataset):
             with open(os.path.join(self.root, "FreiHAND_pub_v2_eval/evaluation_scale.json"), 'r') as f:
                 self.scale = json.load(f)
 
-            with open(os.path.join(self.root, "FreiHAND_pub_v2_eval/evaluation_verts.json"), 'r') as f:
-                self.verts = json.load(f)
+            #with open(os.path.join(self.root, "FreiHAND_pub_v2_eval/evaluation_verts.json"), 'r') as f:
+            #    self.verts = json.load(f)
 
             with open(os.path.join(self.root, "FreiHAND_pub_v2_eval/evaluation_xyz.json"), 'r') as f:
                 self.points = json.load(f)
@@ -51,7 +51,6 @@ class FreiHAND_Dataset(Dataset):
     def __len__(self):
         return len(self.K)
 
-    # TODO: FIX 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
@@ -61,7 +60,7 @@ class FreiHAND_Dataset(Dataset):
         else:
             img_name = os.path.join(self.root, "FreiHAND_pub_v2/training/rgb/", f"{idx:0{8}d}.jpg")
 
-        feature = io.decode_image(img_name)
+        feature = io.decode_image(img_name).to(torch.float32)
         points = torch.tensor(self.points[idx])
         K = torch.tensor(self.K[idx])
         scale = torch.tensor(self.scale[idx])
@@ -71,10 +70,12 @@ class FreiHAND_Dataset(Dataset):
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
+        data, target = data.to(device), target['Points'].to(device)
         optimizer.zero_grad()
         output = model(data)
-        loss = F.nll_loss(output, target)
+        t = target.size()
+        s = output.size()
+        loss = F.mse_loss(output, target, reduction='sum')
         loss.backward()
         optimizer.step()
         if batch_idx % args.log_interval == 0:
@@ -91,11 +92,9 @@ def test(model, device, test_loader):
     correct = 0
     with torch.no_grad():
         for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
+            data, target = data.to(device), target['Points'].to(device)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
+            test_loss += F.mse_loss(output, target, reduction='sum').item()  # sum up batch loss
 
     test_loss /= len(test_loader.dataset)
 
@@ -103,18 +102,9 @@ def test(model, device, test_loader):
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-
-    def forward(self, x):
-        return x
-
 def projectPoints(xyz, K):     
-    xyz = np.array(xyz)     
-    K = np.array(K)     
-    uv = np.matmul(K, xyz.T).T     
-    return uv[:, :2] / uv[:, -1:]
+    p = torch.matmul(K, xyz.t()).t()  
+    return p[:, :2] / p[:, -1:]
 
 def plot_hand(points):
     cmap = {
@@ -128,13 +118,64 @@ def plot_hand(points):
     for finger, params in cmap.items():
         plt.plot(points[params["idx"], 0], points[params["idx"], 1], params["color"])
 
+class ConvBlock(nn.Module):
+    def __init__(self, in_depth, out_depth):
+        super().__init__()
+        self.double_conv = nn.Sequential(
+            nn.BatchNorm2d(in_depth),
+            nn.Conv2d(in_depth, out_depth, kernel_size=3, padding=1, bias=False),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(out_depth),
+            nn.Conv2d(out_depth, out_depth, kernel_size=3, padding=1, bias=False),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+    
+class UNet(nn.Module):
+    def __init__(self, in_channel, out_channel, N):
+        super().__init__()
+        self.convblock1 = ConvBlock(in_channel, N)
+        self.convblock2 = ConvBlock(N, 2*N)
+        self.convblock3 = ConvBlock(2*N, 4*N)
+        self.convblock4 = ConvBlock(4*N, 8*N)
+
+        self.convblock5 = ConvBlock(4*N + 8*N, 4*N)
+        self.convblock6 = ConvBlock(2*N + 4*N, 2*N)
+        self.convblock7 = ConvBlock(N + 2*N, N)
+
+        self.convoutput = nn.Sequential(
+            nn.Conv2d(N, out_channel, kernel_size=3, padding=1, bias=False),
+            nn.Sigmoid()
+        )
+
+        self.maxpool = nn.MaxPool2d(2)
+        self.upsample = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=False)
+        self.flat = nn.Flatten(2, -1)
+        self.output = nn.Linear(224*224, 3)
+        
+    def forward(self, x):
+        conv_x1 = self.convblock1(x)
+        conv_x2 = self.convblock2(self.maxpool(conv_x1))
+        conv_x3 = self.convblock3(self.maxpool(conv_x2))
+        conv_x4 = self.convblock4(self.maxpool(conv_x3))
+
+        conv_u1 = self.convblock5(torch.cat([self.upsample(conv_x4), conv_x3], dim=1))
+        conv_u2 = self.convblock6(torch.cat([self.upsample(conv_u1), conv_x2], dim=1))
+        conv_u3 = self.convblock7(torch.cat([self.upsample(conv_u2), conv_x1], dim=1))
+        conv_l = self.convoutput(conv_u3)
+        flat_l = self.flat(conv_l)
+        return self.output(flat_l)
+        #return self.convoutput(conv_u3) # This is for training against heatmaps
+
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                        help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--batch-size', type=int, default=32, metavar='N',
+                        help='input batch size for training (default: 32)')
+    parser.add_argument('--test-batch-size', type=int, default=32, metavar='N',
+                        help='input batch size for testing (default: 32)')
     parser.add_argument('--epochs', type=int, default=14, metavar='N',
                         help='number of epochs to train (default: 14)')
     parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
@@ -177,20 +218,21 @@ def main():
     training_loader = torch.utils.data.DataLoader(training_dataset,**train_kwargs)
     testing_loader = torch.utils.data.DataLoader(testing_dataset, **test_kwargs)
 
-    i = 0
-    features, labels = next(iter(training_loader))
-    feature = features[i]
-    points = labels['Points'][i]
-    K = labels['K'][i]
-    scale = labels['Scale'][i]
-    imgPoints = projectPoints(points.tolist(), K.tolist()) 
-    img = feature.detach().numpy()
-    img = img.transpose((1, 2, 0))
-    plt.imshow(img)
-    plot_hand(imgPoints)
-    plt.show()
-    return
-    model = Net().to(device)
+
+    #features, labels = next(iter(training_loader))
+    #for i in range (0, len(features)):
+    #    feature = features[i]
+    #    points = labels['Points'][i]
+    #    K = labels['K'][i]
+    #    scale = labels['Scale'][i]
+    #    imgPoints = projectPoints(points, K) 
+    #    img = feature.detach().numpy()
+    #    img = img.transpose((1, 2, 0))
+    #    plt.imshow(img)
+    #    plot_hand(imgPoints)
+    #    plt.show()
+    #return
+    model = UNet(3, 21, 16).to(device)
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
 
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
